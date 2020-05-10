3310d77791589d2c3bc0f39c4e6d2d2d64dbbdb4
deephacks619 <deepakkyatham@gmai.com>
2020-05-10 16:59:59 +0530

    Updates

---
+++ precache-manifest.94fd7df7bef0fd16a8f0173eea7346c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec29de401dfcad4926d8964463810647",
+    "url": "/Recipe-App-Reactjs/index.html"
+  },
+  {
+    "revision": "78e5ada790345ef183fe",
+    "url": "/Recipe-App-Reactjs/static/css/main.6ba085bc.chunk.css"
+  },
+  {
+    "revision": "f8b6b49cd6686504338c",
+    "url": "/Recipe-App-Reactjs/static/js/2.a6e2a2da.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Recipe-App-Reactjs/static/js/2.a6e2a2da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78e5ada790345ef183fe",
+    "url": "/Recipe-App-Reactjs/static/js/main.243071fe.chunk.js"
+  },
+  {
+    "revision": "5a8961fdd5a49137fc6f",
+    "url": "/Recipe-App-Reactjs/static/js/runtime-main.c1a625ea.js"
+  }
+]);
